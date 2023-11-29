ea4a7b3a633ab0ac6fa5bda1696ac8bbd4ed65a7
Hannah Cussen <hcussen6@gmail.com>
2023-11-29 15:17:11 -0800

    MyTinderCard now works

---
+++ components/MyTinderCard.js
@@ -0,0 +1,151 @@
+import React, { useState, useMemo } from "react";
+import { ImageBackground, Text, View, Button } from "react-native";
+import TinderCard from "react-tinder-card";
+
+const styles = {
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  header: {
+    color: "#000",
+    fontSize: 30,
+    marginBottom: 30,
+  },
+  cardContainer: {
+    width: "90%",
+    maxWidth: 260,
+    height: 300,
+  },
+  card: {
+    position: "absolute",
+    backgroundColor: "#fff",
+    width: "100%",
+    maxWidth: 260,
+    height: 300,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+    shadowRadius: 20,
+    borderRadius: 20,
+    resizeMode: "cover",
+  },
+  cardImage: {
+    width: "100%",
+    height: "100%",
+    overflow: "hidden",
+    borderRadius: 20,
+  },
+  cardTitle: {
+    position: "absolute",
+    bottom: 0,
+    margin: 10,
+    color: "#fff",
+  },
+  buttons: {
+    margin: 20,
+    zIndex: -100,
+  },
+  infoText: {
+    height: 28,
+    justifyContent: "center",
+    display: "flex",
+    zIndex: -100,
+  },
+};
+
+const db = [
+  {
+    name: "Richard Hendricks",
+  },
+  {
+    name: "Erlich Bachman",
+  },
+  {
+    name: "Monica Hall",
+  },
+  {
+    name: "Jared Dunn",
+  },
+  {
+    name: "Dinesh Chugtai",
+  },
+];
+
+const alreadyRemoved = [];
+let charactersState = db; // This fixes issues with updating characters state forcing it to use the current state and not the state that was active when the card was created.
+
+export default MyTinderCard = () => {
+  const [characters, setCharacters] = useState(db);
+  const [lastDirection, setLastDirection] = useState();
+
+  const childRefs = useMemo(
+    () =>
+      Array(db.length)
+        .fill(0)
+        .map((i) => React.createRef()),
+    []
+  );
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete + " to the " + direction);
+    setLastDirection(direction);
+    alreadyRemoved.push(nameToDelete);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen!");
+    charactersState = charactersState.filter(
+      (character) => character.name !== name
+    );
+    setCharacters(charactersState);
+  };
+
+  const swipe = (dir) => {
+    const cardsLeft = characters.filter(
+      (person) => !alreadyRemoved.includes(person.name)
+    );
+    if (cardsLeft.length) {
+      const toBeRemoved = cardsLeft[cardsLeft.length - 1].name; // Find the card object to be removed
+      const index = db.map((person) => person.name).indexOf(toBeRemoved); // Find the index of which to make the reference to
+      alreadyRemoved.push(toBeRemoved); // Make sure the next card gets removed next time if this card do not have time to exit the screen
+      childRefs[index].current.swipe(dir); // Swipe the card!
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>React Native Tinder Card</Text>
+      <View style={styles.cardContainer}>
+        {characters.map((character, index) => (
+          <TinderCard
+            ref={childRefs[index]}
+            key={character.name}
+            onSwipe={(dir) => swiped(dir, character.name)}
+            onCardLeftScreen={() => outOfFrame(character.name)}
+          >
+            <View style={styles.card}>
+              {/* <ImageBackground style={styles.cardImage} source={character.img}> */}
+              <Text style={styles.cardTitle}>{character.name}</Text>
+              {/* </ImageBackground> */}
+            </View>
+          </TinderCard>
+        ))}
+      </View>
+      <View style={styles.buttons}>
+        <Button onPress={() => swipe("left")} title="Swipe left!" />
+        <Button onPress={() => swipe("right")} title="Swipe right!" />
+      </View>
+      {lastDirection ? (
+        <Text style={styles.infoText} key={lastDirection}>
+          You swiped {lastDirection}
+        </Text>
+      ) : (
+        <Text style={styles.infoText}>
+          Swipe a card or press a button to get started!
+        </Text>
+      )}
+    </View>
+  );
+};
