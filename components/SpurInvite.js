21b72040c986e6523fa60ce3ea1953f571dbf50f
simrantan <simrant@stanford.edu>
2023-12-02 23:06:01 -0800

    Update SpurInvite.js

--- components/SpurInvite.js
+++ components/SpurInvite.js
@@ -10,19 +10,71 @@ import { Themes } from "../assets/Themes";
 import { Link, Stack } from "expo-router";
 import { millisToMinutesAndSeconds } from "../utils";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import QuickInfo from "./QuickInfo";
 
 const { height: windowHeight, width: windowWidth } = Dimensions.get("window");
+//set table
+const [acceptance, setAcceptance] = useState(null);
 
-export default function Song({
+export default function SpurInvite({
   id,
   activityPhoto,
+  activityTitle,
   friend,
   quickInfo,
   time,
   address,
   isAccepted,
 }) {
-  return <View style={styles.item}></View>;
+  // const updateDB = async (id, acceptance) => {
+  //     const { data, error } = await supabase
+  //       .from(table)
+  //       .update({ isAccepted: acceptance })
+  //       .eq("id", id)
+  //       .select();
+
+  //     if (error) {
+  //       console.log("error here", error);
+  //     } else {
+  //       console.log("successful update, marked:", data);
+  //     }
+  //   };
+  return (
+    <View style={styles.item}>
+      <View style={styles.activityInfo}>
+        <Image style={styles.image}>{activityPhoto}</Image>
+        <View style={styles.activityDetails}>
+          <Text>{activityTitle}</Text>
+          <View style={styles.from}>
+            <Text>From</Text>
+            <Text>{friend.name}</Text>
+          </View>
+          <View style={styles.from}>
+            <Text>When</Text>
+            <Text>{time}</Text>
+            <Text>Where</Text>
+            <Text>{address}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.acceptAndReject}>
+        <Pressable
+          onPress={() => {
+            setAcceptance(true);
+          }}
+        >
+          <Text>Accept</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => {
+            setAcceptance(false);
+          }}
+        >
+          <Text>Reject</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -39,6 +91,16 @@ const styles = StyleSheet.create({
     height: windowWidth * 0.3,
   },
   activityInfo: {
+    flexDirection: "row",
+  },
+  activityDetails: {
+    flexDirection: "column",
+  },
+  acceptAndReject: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  from: {
     flexDirection: "row",
   },
 
